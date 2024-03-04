df469b528b8e4f31a471d8a4d0e76d365aa5543d
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-04 20:55:51 +0100

    Initial Commit

---
+++ Driving Test/Driving_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Driving_TestApp.swift
+//  Driving Test
+//
+//  Created by  Ixart on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Driving_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
