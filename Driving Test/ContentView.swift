1839ea2aad27f55ef70b319f89b84b3f3c207dec
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-06 16:21:50 +0100

    en cours ...

--- Driving Test/ContentView.swift
+++ Driving Test/ContentView.swift
@@ -6,252 +6,141 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
-    @State private var progress = 0.6
+    @State var presentSheet = Bool()
 
     var body: some View {
-        VStack{
-            Text("Driving Test")
-                .font(.title)
-                .bold()
-            Text("Easy to Learn and patrice for you test ")
-                .italic()
-            
-            
-            
-            
-            HStack{
-                ZStack{
-                    Rectangle()
-                        .frame(width:120, height:60)
-                        .foregroundColor(.cyan)
-                        .cornerRadius(30)
+
+        ZStack {
+            Color(.black)
+                
+
+            VStack{
+                
+
+
+                VStack {
+                    Text("Driving Test")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .bold()
+                        .padding(-190)
+                        .padding(.top,-60)
                     
-                    Button(action: {
-                                        
-                                    }, label: {
-                                        
-                                        
-                                        Text("Motorcycle")
-                                            .foregroundStyle(.black)
-                                        
-                                        
-                                        
-                                    })
-               
-                } // FIN ZSTACK
-            
-                
-                
-                ZStack{
-                    Rectangle()
-                        .frame(width:120, height:60)
-                        .foregroundColor(.cyan)
-                        .cornerRadius(30)
                     
-                    Button(action: {
-                                        
-                                    }, label: {
-                                        
-                                        
-                                        Text("car")
-                                            .foregroundStyle(.black)
-                                        
-                                        
-                                        
-                                    })
-               
-                } // FIN ZSTACK
-                
-                
-                ZStack{
-                    Rectangle()
-                        .frame(width:120, height:60)
-                        .foregroundColor(.cyan)
-                        .cornerRadius(30)
+                    Text("Easy to Learn and patrice for you test ")
+                                        .foregroundStyle(.white)
+                                        .italic()
+                                        .padding(-160)
+                                        .padding(.leading,-26)
+                                        .padding(.top,-60)
+
+
                     
-                    Button(action: {
-                                        
-                                    }, label: {
-                                        
-                                        
-                                        Text("Commercial")
-                                            .foregroundStyle(.black)
-                                        
-                                        
-                                        
-                                    })
-                } // FIN ZSTACK
-            } // HSTACK
-            
-            
-            
-            
-            
-            
-            
-            
-            Text("choose your test ")
-                .font(.title)
-                .bold()
-            
-            
-            
+                    
+                    
+                }
+
                 
                 
-            HStack {
-                Button(action: {
-                            
-                        }, label: {
-                            ZStack {
-                               
-                                Rectangle()
-                                    .frame(width: 140, height: 140)
-                                    .cornerRadius(30)
-                                    .foregroundStyle(.black)
-                                Image(systemName: "book")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 50))
-                                    .padding(.top,-30)
-                                    
-                                Text("General")
-                                    .font(.system(size: 22))
-                                    .foregroundStyle(.white)
-                                    .padding(.top,70)
-                            } // ZSTACK
-                    })// Boutton fin label
-                
-                Button(action: {
+                HStack{
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.white)
+                        Rectangle()
+                            .frame(width:120, height:60)
+                            .foregroundColor(.cyan)
+                            .cornerRadius(30)
                         
-                    }, label: {
-                        ZStack {
-                           
-                            Rectangle()
-                                .frame(width: 140, height: 140)
-                                .cornerRadius(30)
-                                .foregroundStyle(.black)
-                            Image(systemName: "chart.bar")
-                                .foregroundColor(.white)
-                                .font(.system(size: 50))
-                                .padding(.top,-30)
-                                
-                            Text("Easy")
-                                .font(.system(size: 22))
-                                .foregroundStyle(.white)
-                                .padding(.top,70)
-                        } // ZSTACK
-                })// Boutton fin label
-            } // FIN HSTACK
-            
-            
-            
-            
-            Text("You Passing Probability:")
-                .font(.title)
-                .bold()
-            
-            
-            
-            ZStack {
+                        Button(action: {
+                            presentSheet = true
+
+                                        }, label: {
+                                            
+                                            
+                                            Text("Motorcycle")
+                                                .foregroundStyle(.black)
+                                            
+                                        })
+                   
+                    } // FIN ZSTACK
+
                 
-                Rectangle()
-                    .frame(width: 330, height: 70)
-                    .cornerRadius(25)
-                    .foregroundColor(.gray)
+                    
+                    ZStack{
+                        Rectangle()
+                            .frame(width:120, height:60)
+                            .foregroundColor(.cyan)
+                            .cornerRadius(30)
+                        
+                        Button(action: {
+                                            
+                                        }, label: {
+                                            
+                                            
+                                            Text("car")
+                                                .foregroundStyle(.black)
+                                            
+                                            
+                                        })
+                   
+                    } // FIN ZSTACK
+                    
+                    
+                    ZStack{
+                        Rectangle()
+                            .frame(width:120, height:60)
+                            .foregroundColor(.cyan)
+                            .cornerRadius(30)
+                        
+                        Button(action: {
+                                            
+                                        }, label: {
+                                            
+                                            
+                                            Text("Commercial")
+                                                .foregroundStyle(.black)
+                                            
+                                            
+                                        })
+                        
+                    } // FIN ZSTACK
+
+                } // HSTACK
                 
                 
-               
                 
                 
-                Rectangle()
-                    .frame(width: 330, height: 70, alignment: .trailing)
-                    .scaleEffect(x: progress, y: 1, anchor: .leading)
-                    .animation(.easeIn, value: progress)
+                
+
+            } // fin vstack
+            .padding(-110)
+
+            .sheet(isPresented: $presentSheet) {
+                NewView()
+                
+
+                            .presentationDetents([.medium, .large])
+                            .presentationDetents([.fraction(0.5)])
 
 
-                    .cornerRadius(25)
-                    .foregroundColor(.blue)
-                
-                Text("65 %")
-                    .foregroundStyle(.black)
-                    .bold()
-                    .font(.title2)
-                
-            } // FIN ZSTACK
-            
-            Text("You passing probality is based on your performance on the app ")
-                .italic()
 
 
-            
-            
-            
-            
-            
-            
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        } // fin vstack
+        }// FIN SHeET
+
+     } // FIN ZSACK
+        .ignoresSafeArea(.all)
+
         
-    } // fin body
+
+        
+        
+  } // fin body
+    
+    
 } // FIN Struct
 #Preview {
     ContentView()
--- Driving Test/NewView.swift
+++ Driving Test/NewView.swift
@@ -8,10 +8,97 @@
 import SwiftUI
 
 struct NewView: View {
+    @State private var progress = 0.6
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        
+        Text("choose your test ")
+            .font(.title)
+            .bold()
+        
+        
+            
+        HStack {
+            Button(action: {
+                        
+                    }, label: {
+                        ZStack {
+                           
+                            Rectangle()
+                                .frame(width: 140, height: 140)
+                                .cornerRadius(30)
+                                .foregroundStyle(.black)
+                            Image(systemName: "book")
+                                .foregroundColor(.white)
+                                .font(.system(size: 50))
+                                .padding(.top,-30)
+                                
+                            Text("General")
+                                .font(.system(size: 22))
+                                .foregroundStyle(.white)
+                                .padding(.top,70)
+                        } // ZSTACK
+                })// Boutton fin label
+            
+            Button(action: {
+                    
+                }, label: {
+                    ZStack {
+                       
+                        Rectangle()
+                            .frame(width: 140, height: 140)
+                            .cornerRadius(30)
+                            .foregroundStyle(.black)
+                        Image(systemName: "chart.bar")
+                            .foregroundColor(.white)
+                            .font(.system(size: 50))
+                            .padding(.top,-30)
+                            
+                        Text("Easy")
+                            .font(.system(size: 22))
+                            .foregroundStyle(.white)
+                            .padding(.top,70)
+                    } // ZSTACK
+            })// Boutton fin label
+        } // FIN HSTACK
+        
+        
+        Text("You Passing Probability:")
+            .font(.title)
+            .bold()
+        
+        
+        ZStack {
+            
+            Rectangle()
+                .frame(width: 330, height: 70)
+                .cornerRadius(25)
+                .foregroundColor(.gray)
+            
+            
+            Rectangle()
+                .frame(width: 330, height: 70, alignment: .trailing)
+                .scaleEffect(x: progress, y: 1, anchor: .leading)
+                .animation(.easeIn, value: progress)
+
+
+                .cornerRadius(25)
+                .foregroundColor(.blue)
+            
+            Text("65 %")
+                .foregroundStyle(.black)
+                .bold()
+                .font(.title2)
+            
+        } // FIN ZSTACK
+        
+        Text("You passing probality is based on your performance on the app ")
+            .italic()
+        
+        
+        
+    } // fin  BODY
+} // FIN STRUC
 
 #Preview {
     NewView()
