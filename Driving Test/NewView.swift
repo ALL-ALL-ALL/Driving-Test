191360e24085456116dd420cb519e82aa21ca7c7
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-06 16:21:06 +0100

    en cours

---
+++ Driving Test/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  Driving Test
+//
+//  Created by  Ixart on 06/03/2024.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
